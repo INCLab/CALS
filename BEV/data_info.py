16e8673d9aa5a366fa72efc10344b700a2ccdb81
Jungik Jang <51084152+cheeseBG@users.noreply.github.com>
2021-10-25 12:20:55 +0900

    Add data_info.py codes

--- BEV/data_info.py
+++ BEV/data_info.py
@@ -2,4 +2,16 @@
     Set the distance(threshold) and two targets.
     Then, find number of frames(times) that two targets are closer than the distance.
 '''
+import sys
+import pandas as pd
+
+threshold = 2
+target_id1 = 1001
+target_id2 = 1002
+
+result = pd.read_csv('tmp/global_result.txt', delimiter=' ', header=None)
+result.columns = ['frame', 'id', 'x', 'y']
+
+print(result)
+
 
