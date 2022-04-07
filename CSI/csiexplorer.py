bff944746ffb14f8f4f3ed9afbb24b732f6d38ae
Jungik Jang <51084152+cheeseBG@users.noreply.github.com>
2022-04-07 21:09:24 +0900

    add gitignore & modify heatmap

--- CSI/csiexplorer.py
+++ CSI/csiexplorer.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 from plot.ampPlotter import AmpPlotter, AmpTimePlotter
-from plot.heatmap import heatmap
+from plot.heatmap import heatmap, timeHeatmap
 
 '''
    <<  Null & Pilot subcarriers on 2.4MHz >>
@@ -14,21 +14,33 @@ from plot.heatmap import heatmap
 # For Test time section, set milestone
 time_ms_list = [
     # ######### Test1 ###########
+    # '2022-04-06 13:55:40',  # Start
+    # '2022-04-06 13:55:47',  # Leave Left-Bottom
+    # '2022-04-06 13:55:53',  # Arrive Right-Top
+    # '2022-04-06 13:55:58',  # Leave Right-Top
+    # '2022-04-06 13:56:01',  # Arrive Right-Bottom
+    # '2022-04-06 13:56:07',  # End
 
     # ######### Test2 ###########
+    # '2022-04-06 16:46:14',  # Start
+    # '2022-04-06 16:46:32',  # Leave Left-Bottom
+    # '2022-04-06 16:46:36',  # Arrive Right-Top
+    # '2022-04-06 16:46:53',  # Leave Right-Top
+    # '2022-04-06 16:46:55',  # Arrive Right-Bottom
+    # '2022-04-06 16:47:22',  # End
 
     # ######### Test3_3rasp ###########
     
-    # '2022-04-06 17:00:00',  # Start
-    # '2022-04-06 17:01:00',  # Leave Left-Bottom
-    # '2022-04-06 17:01:04',  # Arrive Middle-Top
-    # '2022-04-06 17:02:01',  # Leave Middle-Top
-    # '2022-04-06 17:02:04',  # Arrive Right-Bottom
-    # '2022-04-06 17:03:07',  # End
+    '2022-04-06 17:00:00',  # Start
+    '2022-04-06 17:01:00',  # Leave Left-Bottom
+    '2022-04-06 17:01:04',  # Arrive Middle-Top
+    '2022-04-06 17:02:01',  # Leave Middle-Top
+    '2022-04-06 17:02:04',  # Arrive Right-Bottom
+    '2022-04-06 17:03:07',  # End
 ]
 
 # Path
-test_name = 'test1'
+test_name = 'test3_3rasp'
 data_path = '../data'
 data_path = os.path.join(data_path, test_name, 'csi')
 csi_list = os.listdir(data_path)
@@ -76,7 +88,12 @@ if __name__ == "__main__":
     csi_df.drop(null_pilot_col_list, axis=1, inplace=True)
 
     while True:
-        plot_mode = input('1.Amplitude-SampleNum plot 2.Amplitude-Time plot 3.Heatmap, 4.Exit: ')
+        plot_mode = input('1.Amplitude-SampleNum plot\n'
+                          '2.Amplitude-Time plot\n'
+                          '3.Heatmap\n'
+                          '4.Time-Heatmap\n'
+                          '5.Exit\n'
+                          'Select: ')
 
         if plot_mode == '1':
             # select specific subcarrier
@@ -119,8 +136,17 @@ if __name__ == "__main__":
                 heatmap(csi_df, s_start, s_end)
             else:
                 print("Wrong input!")
-
         elif plot_mode == '4':
+            pre = input('Data Preprocessing (True or False):  ')
+
+            if pre == 'True':
+                timeHeatmap(csi_df, time_list, time_ms_list, preprocess=True)
+            elif pre == 'False':
+                timeHeatmap(csi_df, time_list, time_ms_list)
+            else:
+                print("Wrong input!")
+
+        elif plot_mode == '5':
             break
         else:
             print('Unknown command.')
