be347c40abf505bf697b8d966ba47013cc97c049
Jungik Jang <51084152+cheeseBG@users.noreply.github.com>
2022-04-15 23:20:57 +0900

    Update ping_cmd.py

--- edge/ping_cmd.py
+++ edge/ping_cmd.py
@@ -6,13 +6,19 @@ try:
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy)
     ssh.connect(ip, username="inclab", password="1q2w3e4r0110!")
     print("{} is connected".format(ip))
-    stdin, stdout, dtderr = ssh.exec_command("ls -l")
 
-    lines = stdout.readlines()
-    for i in lines:
-        re = str(i).replace('\n','')
-        print(re)
-    ssh.close()
+    channel = ssh_client.invoke_shell()
+    channel.send('sudo su -\n')
+    outdata, errdata = waitStrems(channel)
+    print(outdata)
+
+    channel.send('1q2w3e4r0110!\n')
+    outdata, errdata = waitStrems(channel)
+    print(outdata)
+
+    channel.send('whoami\n')
+    outdata, errdata = waitStrems(channel)
+    print(outdata)
 
 except Exception as err:
     print(err)
