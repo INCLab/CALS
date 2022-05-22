c76e2f769cdb6c3d2973c96f8ffc55dd251ce11a
Jungik Jang <51084152+cheeseBG@users.noreply.github.com>
2022-05-22 16:36:22 +0900

    Update cnn1d.py

--- model/cnn1d.py
+++ model/cnn1d.py
@@ -14,61 +14,84 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 dataPath = '../data/pe'
 
-# Load Person Exist dataset
-pe_df, npe_df = DataLoader().loadWindowPeData(dataPath)
+null_pilot_col_list = ['_' + str(x + 32) for x in [-32, -31, -30, -29, -21, -7, 0, 7, 21, 29, 30, 31]]
+total_sub = list(np.arange(0, 63, 1))
+sub_list = []
+for sub in total_sub:
+    sub = '_' + str(sub)
+    if sub not in null_pilot_col_list:
+        sub_list.append(sub)
 
-csi_df = pd.concat([pe_df, npe_df], ignore_index=True)
+max_acc = 0
+max_sub_idx = None
+acc_list = []
 
-print('< PE data size > \n {}'.format(len(pe_df)))
-print('< NPE data size > \n {}'.format(len(npe_df)))
+for sub in sub_list:
+    # Load Person Exist dataset
+    pe_df, npe_df = DataLoader().loadWindowPeData(dataPath, [sub])
 
+    csi_df = pd.concat([pe_df, npe_df], ignore_index=True)
 
-# Divide feature and label
-csi_data, csi_label = csi_df.iloc[:, :-1], csi_df.iloc[:, -1]
-
-# Divide Train, Test dataset
-X_train, X_test, y_train, y_test = train_test_split(csi_data, csi_label, test_size=0.2, random_state=42)
-
-# Change to ndarray
-X_train = np.array(X_train)
-X_test = np.array(X_test)
-y_train = np.array(y_train)
-y_test = np.array(y_test)
-
-# # Sampling
-# SAMPLE_NUM = 8000=
-# X_train, y_train = X_train[:SAMPLE_NUM], y_train[:SAMPLE_NUM]
-# X_test, y_test = X_test[:int(SAMPLE_NUM * 0.2)], y_test[:int(SAMPLE_NUM * 0.2)]
+    print('< PE data size > \n {}'.format(len(pe_df)))
+    print('< NPE data size > \n {}'.format(len(npe_df)))
 
 
-print('X shape: {}'.format(X_train.shape))
-print('y shape: {}'.format(y_train.shape))
+    # Divide feature and label
+    csi_data, csi_label = csi_df.iloc[:, :-1], csi_df.iloc[:, -1]
 
-X_train = X_train.reshape((X_train.shape[0], X_train.shape[1], 1))  # LSTM은 input으로 3차원 (datasize, timestamp, feature)
-print('X reshape: {}'.format(X_train.shape))
+    # Divide Train, Test dataset
+    X_train, X_test, y_train, y_test = train_test_split(csi_data, csi_label, test_size=0.2, random_state=42)
 
-X_test = X_test.reshape((X_test.shape[0], X_test.shape[1], 1))
+    # Change to ndarray
+    X_train = np.array(X_train)
+    X_test = np.array(X_test)
+    y_train = np.array(y_train)
+    y_test = np.array(y_test)
 
-TIMESTEMP = 10
+    # # Sampling
+    # SAMPLE_NUM = 8000=
+    # X_train, y_train = X_train[:SAMPLE_NUM], y_train[:SAMPLE_NUM]
+    # X_test, y_test = X_test[:int(SAMPLE_NUM * 0.2)], y_test[:int(SAMPLE_NUM * 0.2)]
 
-dropout_ratio = 0.3 # 드롭아웃 비율
-num_filters = 256 # 커널의 수
-kernel_size = 10 # 커널의 크기
-hidden_units = 128 # 뉴런의 수
 
-model = Sequential()
-#model.add(Dropout(dropout_ratio))
-model.add(Conv1D(num_filters, kernel_size, padding='valid', activation='relu'))
-model.add(GlobalMaxPooling1D())
-model.add(Dense(hidden_units, activation='relu'))
-#model.add(Dropout(dropout_ratio))
-model.add(Dense(1, activation='sigmoid'))
+    print('X shape: {}'.format(X_train.shape))
+    print('y shape: {}'.format(y_train.shape))
 
-es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=3)
-mc = ModelCheckpoint('best_model.h5', monitor='val_acc', mode='max', verbose=1, save_best_only=True)
+    X_train = X_train.reshape((X_train.shape[0], X_train.shape[1], 1))  # LSTM은 input으로 3차원 (datasize, timestamp, feature)
+    print('X reshape: {}'.format(X_train.shape))
 
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
-history = model.fit(X_train, y_train, epochs=20, batch_size=64, validation_data=(X_test, y_test), callbacks=[es, mc])
+    X_test = X_test.reshape((X_test.shape[0], X_test.shape[1], 1))
 
-loaded_model = load_model('best_model.h5')
-print("\n 테스트 정확도: %.4f" % (loaded_model.evaluate(X_test, y_test)[1]))
+    TIMESTEMP = 10
+
+    dropout_ratio = 0.3 # 드롭아웃 비율
+    num_filters = 256 # 커널의 수
+    kernel_size = 10 # 커널의 크기
+    hidden_units = 128 # 뉴런의 수
+
+    model = Sequential()
+    #model.add(Dropout(dropout_ratio))
+    model.add(Conv1D(num_filters, kernel_size, padding='valid', activation='relu'))
+    model.add(GlobalMaxPooling1D())
+    model.add(Dense(hidden_units, activation='relu'))
+    #model.add(Dropout(dropout_ratio))
+    model.add(Dense(1, activation='sigmoid'))
+
+    es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=3)
+    mc = ModelCheckpoint('best_model.h5', monitor='val_acc', mode='max', verbose=1, save_best_only=True)
+
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
+    history = model.fit(X_train, y_train, epochs=20, batch_size=64, validation_data=(X_test, y_test), callbacks=[es, mc])
+
+    loaded_model = load_model('best_model.h5')
+
+    acc = loaded_model.evaluate(X_test, y_test)[1]
+    print("\n 테스트 정확도: %.4f" % (acc))
+
+    acc_list.append([sub, acc])
+    if result[-1] > max_acc:
+        max_acc = result[-1]
+        max_sub_idx = sub
+
+print('max acc: {}, sub_idx: {}'.format(max_acc, max_sub_idx))
+print(acc_list.sort(key=lambda x: (x[1], x[0])))
