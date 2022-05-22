2c61c873a4f74cdcc72e3516b5d80d55ea2d5d97
Jungik Jang <51084152+cheeseBG@users.noreply.github.com>
2022-05-22 12:58:51 +0900

    Update LSTM.py

--- model/LSTM.py
+++ model/LSTM.py
@@ -5,6 +5,7 @@ from numpy import array
 from keras.models import Sequential
 from keras.layers import Dense, LSTM
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
 
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
@@ -33,7 +34,7 @@ y_train = np.array(y_train)
 y_test = np.array(y_test)
 
 # # Sampling
-# SAMPLE_NUM = 8000=
+# SAMPLE_NUM = 8000
 # X_train, y_train = X_train[:SAMPLE_NUM], y_train[:SAMPLE_NUM]
 # X_test, y_test = X_test[:int(SAMPLE_NUM * 0.2)], y_test[:int(SAMPLE_NUM * 0.2)]
 
@@ -47,12 +48,10 @@ X_train = X_train.reshape((X_train.shape[0], X_train.shape[1], 1))  # LSTM은 in
 print('X reshape: {}'.format(X_train.shape))
 
 model = Sequential()
-model.add(LSTM(128, input_shape=(TIMESTEMP, 1)))
+model.add(LSTM(256, input_shape=(TIMESTEMP, 1)))
 #model.add(LSTM(10, activation='relu', return_sequences=True))  # (None, TIMESTEMP, 10)을 받는다
 #model.add(LSTM(3, activation='relu'))  # 마지막은 return_sequence X
-# return_sequence를 쓰면 dimension이 한개 추가 되므로 다음 Dense Layer의 인풋에 3 dim이 들어가게 되므로 안씀
-# LSTM 두개를 엮을 때
-#model.add(Dense(5, activation='relu'))
+model.add(Dense(128, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 
 model.summary()
@@ -62,9 +61,13 @@ model.compile(optimizer='adam', loss='binary_crossentropy', metrics='accuracy')
 from keras.callbacks import EarlyStopping
 
 early_stopping = EarlyStopping(monitor='loss', patience=100, mode='auto')
-model.fit(X_train, y_train, epochs=100, batch_size=32, verbose=2, callbacks=[early_stopping])
+model.fit(X_train, y_train, epochs=50, batch_size=32, verbose=2, callbacks=[early_stopping])
 
 X_test = X_test.reshape((X_test.shape[0], X_test.shape[1], 1))
 
 result = model.evaluate(X_test, y_test)
 print(result)
+
+pred = model.predict(X_test)
+
+print(classification_report(y_test, pred))
