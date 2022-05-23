2949c09209dc955334c452313dd653bd06a6e011
Jungik Jang <51084152+cheeseBG@users.noreply.github.com>
2022-05-24 01:26:54 +0900

    update

--- model/LSTM.py
+++ model/LSTM.py
@@ -76,16 +76,39 @@ model.summary()
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics='accuracy')
 
 early_stopping = EarlyStopping(monitor='val_loss', mode='min', patience=10)
-model.fit(X_train, y_train, epochs=50, batch_size=32,  validation_data=(X_test, y_test), verbose=2, callbacks=[early_stopping])
+history = model.fit(X_train, y_train, epochs=50, batch_size=32,  validation_data=(X_test, y_test), verbose=2, callbacks=[early_stopping])
 
 X_test = X_test.reshape((X_test.shape[0], X_test.shape[1], 1))
 
 result = model.evaluate(X_test, y_test)
 print(result)
 
-# if result[-1] > max_acc:
-#     max_acc = result[-1]
-#     max_sub_idx = sub
+import matplotlib.pyplot as plt
 
-# print('max acc: {}, sub_idx: {}'.format(max_acc, max_sub_idx))
-# print(acc_list)
+history_dict = history.history
+loss = history_dict['loss']
+val_loss = history_dict['val_loss']
+
+epochs = range(1, len(loss) + 1)
+
+plt.plot(epochs, loss, 'bo', label='Training loss')  # ‘bo’는 파란색 점을 의미합니다.
+plt.plot(epochs, val_loss, 'b', label='Validation loss') # ‘b’는 파란색 실선을 의미합니다.
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.show()
+
+plt.clf() # 그래프를 초기화합니다.
+acc = history_dict['acc']
+val_acc = history_dict['val_acc']
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+
+plt.show()
